581ca9ac55d0702c46d667b67440646e4b49fb31
Cupid Valentine <westerngod@126.com>
2023-03-26 23:16:53 +0800

    refactor: update open ai params

--- src/pages/api/askQuestion.ts
+++ src/pages/api/askQuestion.ts
@@ -4,9 +4,6 @@ import { query } from '@/lib/queryApi'
 import { adminDB } from '@/utils/firebaseAdmin'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export const config = {
-  runtime: 'edge'
-}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { prompt, chatId, model, session } = req.body
