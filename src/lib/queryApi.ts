a4d031b0b569d95d3b6feb3e3bc08c10078fad40
Cupid Valentine <westerngod@126.com>
2023-03-26 21:37:49 +0800

    refactor: optimized images visible

--- src/lib/queryApi.ts
+++ src/lib/queryApi.ts
@@ -6,11 +6,11 @@ export const query = async (prompt: string, chatId: string, model: string) => {
     .createChatCompletion({
       model,
       messages: [{ role: 'user', content: prompt }],
-      temperature: 0.9
-      // top_p: 1,
-      // max_tokens: 1000,
-      // frequency_penalty: 0,
-      // presence_penalty:0
+      temperature: 0.9,
+      top_p: 1,
+      max_tokens: 1000,
+      frequency_penalty: 0,
+      presence_penalty:0
     })
     .then((res) => res.data.choices[0].message?.content)
     .catch((err) => `ChatGPT was unable to find an answer for that! (Error: ${err.message})`)
